4f9c57f054a5034adefabe81abee19c3de131aa5
Paulo Salgado <paulo.salgado@alfaerp.com.br>
2025-10-05 18:23:42 +0000

    Commit Inicial

---
+++ webapp/controller/Cockpit.controller.ts
@@ -0,0 +1,12 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace com.alfa.cockpit.controller
+ */
+export default class Cockpit extends Controller {
+
+    /*eslint-disable @typescript-eslint/no-empty-function*/
+    public onInit(): void {
+
+    }
+}
