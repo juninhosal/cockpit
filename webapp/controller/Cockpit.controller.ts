4ba577bda765873c0879bcf8c0919cf38b7673fd
Paulo de Tarso Salgado Junior <junior.salgado@outlook.com>
2025-10-05 17:26:45 -0300

    Commit - Menu

--- webapp/controller/Cockpit.controller.ts
+++ webapp/controller/Cockpit.controller.ts
@@ -1,12 +1,23 @@
 import Controller from "sap/ui/core/mvc/Controller";
+import ToolPage from "sap/tnt/ToolPage";
 
 /**
  * @namespace com.alfa.cockpit.controller
  */
 export default class Cockpit extends Controller {
 
-    /*eslint-disable @typescript-eslint/no-empty-function*/
     public onInit(): void {
-
+        // Seu código de inicialização
     }
+
+    /**
+     * Alterna a visibilidade do menu lateral.
+     */
+    public onMenuButtonPress(): void {
+        const oToolPage = this.byId("toolPage") as ToolPage;
+        oToolPage.setSideExpanded(!oToolPage.getSideExpanded());
+    }
+
+
+
 }
---
+++ webapp/controller/Users.controller.ts
@@ -0,0 +1,11 @@
+// webapp/controller/Users.controller.ts
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace com.alfa.cockpit.controller
+ */
+export default class Users extends Controller {
+    public onInit(): void {
+        // Lógica de inicialização para a página de usuários
+    }
+}
